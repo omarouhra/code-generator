354cf90814ba914df59ba1924c276e64964056d9
Omar Ouhra <omarmaroc26@gmail.com>
2024-03-06 12:39:00 +0100

    feat: starter code patterns

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
+const PATTERNS = [
+  { key: "easy", pattern: "0123456789" },
+  {
+    key: "strong",
+    pattern: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+  },
+  {
+    key: "veryStrong",
+    pattern: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+  },
+];
+
 function App() {
   return <div>Amigoscode React App Template</div>;
 }
